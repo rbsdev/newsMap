9e338da734f57601452470e8352032d337daeeae
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2014-03-23 05:46:38 -0300

    solved problems with views of news detail

--- news_map/contents/views.py
+++ news_map/contents/views.py
@@ -71,7 +71,8 @@ def new_detail_view(request, id):
   new = New.objects.get(id=id)
   new.__dict__.pop('_state', None)
   new.__dict__.pop('date', None)
-  import pdb; pdb.set_trace()
+  new.__dict__.pop('image', None)
+
   data = json.dumps(new.__dict__)
 
   return HttpResponse(data, mimetype='application/json')
