aa4bd78ed3258c34c50aa3aed420554497bbdb67
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2014-03-23 02:04:31 -0300

    created views

--- news_map/contents/urls.py
+++ news_map/contents/urls.py
@@ -5,4 +5,6 @@ from contents import views
 urlpatterns = patterns('',
   url(r'^$', views.index_view, name='index'),
   url(r'^news/(?P<total>\d{1,2})/$', views.news_view, name='news'),
+  url(r'^agendas/(?P<total>\d{1,2})/$', views.agendas_view, name='agendas'),
+  url(r'^places/(?P<total>\d{1,2})/$', views.places_view, name='places'),
 )
--- news_map/contents/views.py
+++ news_map/contents/views.py
@@ -1,19 +1,44 @@
 import json
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
-from contents.models import New
+from contents.models import New, Agenda, Place
+
 
 def index_view(request):
   return render_to_response('contents/index.html')
+
+
+def convert_to_json(obj):
+  has_data = len(obj) > 0
+
+  if not has_data:
+    return HttpResponse(status=404)
+
+  data = json.dumps(list(obj))
+
+  return HttpResponse(data, mimetype='application/json')
+
 
 def news_view(request, total=1):
   start = 0
   news = New.objects.all()[start:total].\
                           values('id', 'title', 'lat', 'lon')
 
-  if not news:
-    return HttpResponse(status=404)
+  return convert_to_json(news)
 
-  data = json.dumps(list(news))
 
-  return HttpResponse(data, mimetype='application/json')
+def agendas_view(request, total=1):
+  start = 0
+  agendas = Agenda.objects.all()[start:total].\
+                          values('id', 'title', 'lat', 'lon', 'description',\
+                                 'start_date', 'finish_date')
+
+  return convert_to_json(agendas)
+
+
+def places_view(request, total=1):
+  start = 0
+  places = Place.objects.all()[start:total].\
+                          values('id', 'title', 'lat', 'lon')
+
+  return convert_to_json(places)
